9f935ad19eaee4399b993b867cded5a077a79459
tomoyuki <at01110833@gmail.com>
2023-04-23 04:33:36 +0900

    カレンダープログラム課題修正

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -3,9 +3,7 @@
 require "date"
 require "optparse"
 
-date = Date.today
-month = date.month
-year = date.year
+today = Date.today
 
 option = {}
 opt = OptionParser.new
@@ -13,22 +11,22 @@ opt.on("-m MONTH") {|m| option[:m] = m}
 opt.on("-y YEAR") {|y| option[:y] = y}
 opt.parse(ARGV)
 
-month = option[:m].to_i if option[:m]
-year = option[:y].to_i if option[:y]
+month = option[:m] ? option[:m].to_i : today.month
+year = option[:y] ? option[:y].to_i : today.year
 
-week = Date.new(year, month, 1).wday
-first_day = Date.new(year, month, 1).day
-last_day = Date.new(year, month, -1).day
+wday = Date.new(year, month, 1).wday
+first_day = Date.new(year, month, 1)
+last_day = Date.new(year, month, -1)
 
 puts "#{month}月 #{year}年".center(22)
 puts " 日 月 火 水 木 金 土"
-print "   " * week
+print "   " * wday
 
-(first_day..last_day).each do |day|
-  print day.to_s.rjust(3)
-  week += 1
-  if week % 7 == 0
+(first_day..last_day).each do |date|
+  print date.day.to_s.rjust(3)
+  if date.saturday?
     print "\n"
   end
 end
+print "\n"
 
