71dfae55f1c667b09c26ad633b0c7f7ed2f48235
tomoyuki <at01110833@gmail.com>
2023-04-24 10:57:44 +0900

    スペースと変数の寿命の修正

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -3,14 +3,13 @@
 require "date"
 require "optparse"
 
-today = Date.today
-
 option = {}
 opt = OptionParser.new
 opt.on("-m MONTH") {|m| option[:m] = m}
 opt.on("-y YEAR") {|y| option[:y] = y}
 opt.parse(ARGV)
 
+today = Date.today
 month = option[:m] ? option[:m].to_i : today.month
 year = option[:y] ? option[:y].to_i : today.year
 
@@ -18,12 +17,12 @@ wday = Date.new(year, month, 1).wday
 first_day = Date.new(year, month, 1)
 last_day = Date.new(year, month, -1)
 
-puts "#{month}月 #{year}年".center(22)
-puts " 日 月 火 水 木 金 土"
+puts "#{month}月 #{year}年".center(20)
+puts "日 月 火 水 木 金 土"
 print "   " * wday
 
 (first_day..last_day).each do |date|
-  print date.day.to_s.rjust(3)
+  print date.day.to_s.rjust(2) + " "
   if date.saturday?
     print "\n"
   end
