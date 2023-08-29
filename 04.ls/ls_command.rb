98162a2ed71c94d1b2b8243b29233509c3a374f1
tomoyuki <at01110833@gmail.com>
2023-08-29 14:19:16 +0900

    lsコマンドを作る4の課題

--- 04.ls/ls_command.rb
+++ 04.ls/ls_command.rb
@@ -2,20 +2,41 @@
 # frozen_string_literal: true
 
 require 'optparse'
+require 'etc'
 
 COLUMN = 3
+
+PERMISSION = {
+  0 => '---',
+  1 => '--x',
+  2 => '-w-',
+  3 => '-wx',
+  4 => 'r--',
+  5 => 'r-x',
+  6 => 'rw-',
+  7 => 'rwx'
+}.freeze
+
+FILE_TYPE = {
+  'fifo' => 'p',
+  'characterSpecial' => 'c',
+  'directory' => 'd',
+  'blockSpecial' => 'b',
+  'file' => '-',
+  'link' => 'l',
+  'socket' => 's'
+}.freeze
 
 def filenames
   Dir.glob('*')
 end
 
-def chunk_filenames(unchunked_filenames, options)
+def chunk_filenames(unchunked_filenames)
   file_length = unchunked_filenames.length
   file_row = (file_length / COLUMN.to_f).ceil
 
   files = []
-  target_filenames = options['r'] ? unchunked_filenames.reverse : unchunked_filenames
-  target_filenames.each_with_index do |file_name, index|
+  unchunked_filenames.each_with_index do |file_name, index|
     row = index % file_row
     files[row] ||= []
     files[row] << file_name
@@ -33,14 +54,45 @@ def output(chunked_filenames)
   end
 end
 
+def permission(mode)
+  owner = PERMISSION[mode[-3].to_i]
+  group = PERMISSION[mode[-2].to_i]
+  other = PERMISSION[mode[-1].to_i]
+  "#{owner}#{group}#{other}"
+end
+
+def block_number
+  sum = 0
+  filenames.each do |filename|
+    stats = File::Stat.new(filename)
+    sum += stats.blocks
+  end
+  puts "total #{sum}"
+end
+
+def ls_l
+  block_number
+  filenames.each do |filename|
+    stats = File::Stat.new(filename)
+    file_type = FILE_TYPE[File.ftype(filename)]
+    permissions = permission(stats.mode.to_s(8))
+    hard_link = stats.nlink
+    owner = Etc.getpwuid(stats.uid).name
+    group = Etc.getgrgid(stats.gid).name
+    size = stats.size
+    time = stats.mtime.strftime('%_m %d %H:%M')
+    puts "#{file_type}#{permissions}  #{hard_link} #{owner}  #{group} #{size.to_s.rjust(5)} #{time} #{filename}"
+  end
+end
+
 def parse_options
-  ARGV.getopts('r')
+  ARGV.getopts('l')
 end
 
 def exec_ls_command
   options = parse_options
-  chunked_filenames = chunk_filenames(filenames, options)
-  output(chunked_filenames)
+  chunked_filenames = chunk_filenames(filenames)
+  options['l'] ? ls_l : output(chunked_filenames)
 end
 
 exec_ls_command
