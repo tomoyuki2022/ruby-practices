3bdd1adfc0e505a66d11cb0ce0bf3296cfb124af
tomoyuki <at01110833@gmail.com>
2023-09-22 16:03:48 +0900

    課題修正

--- 05.wc/wc_command.rb
+++ 05.wc/wc_command.rb
@@ -7,27 +7,21 @@ def parse_filenames
   ARGV
 end
 
-def input_text
-  gets('')
-end
-
-def read_output_file
+def input_text_or_read_file
   $stdin.read
 end
 
-def target_count_output(file_or_text, options)
+def output_target_count(file_or_text, options)
   line = file_or_text.count("\n")
   word = file_or_text.split(/\s+/).size
-  byte = file_or_text.size
+  byte = file_or_text.bytesize
+  length = [line, word, byte].max.to_s.length
+  space = length < 8 ? 8 : length + 3
   output = []
-  output << line.to_s.rjust(8) if options[:l]
-  output << word.to_s.rjust(8) if options[:w]
-  output << byte.to_s.rjust(8) if options[:c]
+  output << line.to_s.rjust(space) if options[:l]
+  output << word.to_s.rjust(space) if options[:w]
+  output << byte.to_s.rjust(space) if options[:c]
   puts output.join('')
-end
-
-def target
-  $stdin.tty? ? read_output_file : input_text
 end
 
 def count(filenames)
@@ -38,38 +32,42 @@ def count(filenames)
     readed_file = File.read(filename)
     lines += readed_file.count("\n")
     words += readed_file.split(/\s+/).size
-    bytes += readed_file.size
+    bytes += readed_file.bytesize
   end
-  [lines, words, bytes]
+  { line: lines, word: words, byte: bytes }
 end
 
 def print_file_count(filename, count, options)
+  length = count.values.max.to_s.length
+  space = length < 8 ? 8 : length + 3
   output = []
-  output << count[0].to_s.rjust(8) if options[:l]
-  output << count[1].to_s.rjust(8) if options[:w]
-  output << count[2].to_s.rjust(8) if options[:c]
+  output << count[:line].to_s.rjust(space) if options[:l]
+  output << count[:word].to_s.rjust(space) if options[:w]
+  output << count[:byte].to_s.rjust(space) if options[:c]
   output << " #{filename}"
   puts output.join('')
 end
 
 def print_total_count(total_count, options)
+  length = total_count.values.max.to_s.length
+  space = length < 8 ? 8 : length + 3
   output = []
-  output << total_count[0].to_s.rjust(8) if options[:l]
-  output << total_count[1].to_s.rjust(8) if options[:w]
-  output << total_count[2].to_s.rjust(8) if options[:c]
+  output << total_count[:line].to_s.rjust(space) if options[:l]
+  output << total_count[:word].to_s.rjust(space) if options[:w]
+  output << total_count[:byte].to_s.rjust(space) if options[:c]
   puts "#{output.join('')} total"
 end
 
-def count_stats_output(options)
+def output_count_stats(options)
   filenames = parse_filenames
-  total_counts = [0, 0, 0]
+  total_counts = { line: 0, word: 0, byte: 0 }
   filenames.each do |filename|
     counts = count(filename)
     print_file_count(filename, counts, options)
 
-    total_counts[0] += counts[0]
-    total_counts[1] += counts[1]
-    total_counts[2] += counts[2]
+    total_counts[:line] += counts[:line]
+    total_counts[:word] += counts[:word]
+    total_counts[:byte] += counts[:byte]
   end
   print_total_count(total_counts, options) if filenames[1]
 end
@@ -88,7 +86,7 @@ unless options.values.any?
 end
 
 def execution_wc_command(options)
-  ARGV.any? ? count_stats_output(options) : target_count_output(target, options)
+  !ARGV.empty? ? output_count_stats(options) : output_target_count(input_text_or_read_file, options)
 end
 
 execution_wc_command(options)
