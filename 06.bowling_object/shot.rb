775b194b302facefd5e18353b3ea502a4ab05ef3
tomoyuki <at01110833@gmail.com>
2024-05-22 14:57:20 +0900

    定数の利用と三項演算子の利用に修正

--- 06.bowling_object/shot.rb
+++ 06.bowling_object/shot.rb
@@ -2,13 +2,13 @@
 
 # マークの"X"の場合10を返し、その他は整数で返す
 class Shot
+  STRIKE_MARK = 'X'
+
   def initialize(mark)
     @mark = mark
   end
 
   def score
-    return 10 if @mark == 'X'
-
-    @mark.to_i
+    @mark == STRIKE_MARK ? 10 : @mark.to_i
   end
 end
