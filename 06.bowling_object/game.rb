45502624747066f208a7b35478cf6a7b6b1b423e
tomoyuki <at01110833@gmail.com>
2024-05-24 16:18:57 +0900

    scoreメソッドの初期値の定義をなくした。build_frames_from_shotsメソッドをインスタンス変数に代入し、@shotsをなくした。calculate_frame_scoresメソッドで早期returnを使用した。

--- 06.bowling_object/game.rb
+++ 06.bowling_object/game.rb
@@ -4,67 +4,57 @@ require_relative 'frame'
 
 # スコアのトータルを計算する
 class Game
-  def initialize(shots)
-    @shots = shots
+  def initialize
+    @frames = build_frames_from_shots
   end
 
   def score
-    total_score = 0
-    build_frames_from_shots.each.with_index do |frame_shot, i|
-      total_score += calculate_frame_scores(frame_shot, i)
-    end
+    total_score = @frames.each.with_index.sum { |frame_shot, i| calculate_frame_scores(frame_shot, i) }
     puts total_score
   end
 
   private
 
   def build_frames_from_shots
+    shots = ARGV[0].split(',')
     frames = []
     i = 0
-    i = add_shot_to_frames(frames, @shots, i) while i < @shots.size
+    while i < shots.size
+      i, frame = add_shot_to_frames(shots, i)
+      frames << frame
+    end
     frames
   end
 
-  def add_shot_to_frames(frames, shots, index)
+  def add_shot_to_frames(shots, index)
     if shots[index] == Shot::STRIKE_MARK
-      frames << Frame.new(shots[index])
-      index + 1
+      frame = Frame.new(shots[index])
+      [index + 1, frame]
     else
-      frames << Frame.new(shots[index], shots[index + 1])
-      index + 2
+      frame = Frame.new(shots[index], shots[index + 1])
+      [index + 2, frame]
     end
   end
 
   def calculate_frame_scores(frame, index)
-    if index < 9
-      calculate_frame_points(frame, index)
-    else
-      frame.score
-    end
-  end
+    return frame.score if index >= 9
+    return frame.score + calculate_strike_points(index) if frame.strike?
+    return frame.score + calculate_spare_points(index) if frame.spare?
 
-  def calculate_frame_points(frame, index)
-    if frame.strike?
-      frame.score + calculate_strike_points(index)
-    elsif frame.spare?
-      frame.score + calculate_spare_points(index)
-    else
-      frame.score
-    end
+    frame.score
   end
 
   def calculate_strike_points(index)
-    next_frame = build_frames_from_shots[index + 1]
-    next_next_frame = build_frames_from_shots[index + 2]
-    connected_frame_scores = next_frame.shots + (next_next_frame ? next_next_frame.shots : [])
+    next_frame = @frames[index + 1]
+    next_next_frame = @frames[index + 2]
+    connected_frame_scores = next_frame.shots + (next_next_frame&.shots || [])
     connected_frame_scores.slice(0, 2).sum(&:score)
   end
 
   def calculate_spare_points(index)
-    build_frames_from_shots[index + 1].shots.first.score
+    @frames[index + 1].shots.first.score
   end
 end
 
-shots = ARGV[0].split(',')
-game = Game.new(shots)
+game = Game.new
 game.score
